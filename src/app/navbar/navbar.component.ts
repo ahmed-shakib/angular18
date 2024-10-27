23c471d60fa1bb9f569b22e207cf2c05b45615c6
Shakib Ahmed <ahmed-shakib@outlook.com>
2024-10-27 16:16:30 -0400

    Added html and css code in the component file in multiple lines

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,9 +3,12 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    template:'<h1>Navbar Component</h1>'
+    template:`<h1>Navbar Component</h1>
+              <p>Dummy paragraph</p>
+              <button>Submit</button>`,
+    styles: `h1{color:red;
+                background:black                 
+            }`
 })
 
-export class AppNavbar{
-
-}
+export class AppNavbar{}
