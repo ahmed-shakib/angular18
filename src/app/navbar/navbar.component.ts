cdce10a1afca539a1009adcf3a636474ff9f81d4
Shakib Ahmed <ahmed-shakib@outlook.com>
2024-10-28 19:04:48 -0400

    Added separate html and scss file and linked with the component file

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,13 +2,9 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-navbar',
-    standalone: true,
-    template:`<h1>Navbar Component</h1>
-              <p>Dummy paragraph</p>
-              <button>Submit</button>`,
-    styles: `h1{color:red;
-                background:black                 
-            }`
+    standalone: true,   
+    templateUrl:'./navbar.component.html',
+    styleUrl:'./navbar.component.scss',
 })
 
 export class AppNavbar{}
